a1e4ce99c174bdea0805047dadf15485c873ffcb
mangostaninja <piuquen.t@gmail.com>
2021-05-10 19:00:28 +0000

    correccion de idioma

--- dominios.js
+++ dominios.js
@@ -1,16 +1,16 @@
- let pronombre = ['el', 'la', 'tu'];
-  let adjetivo = ['gran', 'mejor', 'pequeño'];
-  let objeto = ['dominio','mapache', 'monitor'];
+ let pronoun = ['the', 'our', 'your'];
+  let adjective = ['big', 'best', 'little'];
+  let object = ['domain','racoon', 'monitor'];
 
-     function concatenar (pro, adj, obj){
+     function concatente (pro, adj, obj){
 
-        for (count1 = 0; count1 < pronombre.length; count1++) {
-            for (count2 = 0; count2 < adjetivo.length; count2++){
-                for (count3 = 0; count3 < pronombre.length; count3++){
-                    console.log(pronombre[count1] + adjetivo[count2] + objeto[count3] + '.com');
+        for (count1 = 0; count1 < pronoun.length; count1++) {
+            for (count2 = 0; count2 < adjective.length; count2++){
+                for (count3 = 0; count3 < pronoun.length; count3++){
+                    console.log(pronoun[count1] + adjective[count2] + object[count3] + '.com');
                 }
             }
         }
 
     };
-    concatenar(pronombre, adjetivo, objeto);
+    concatente(pronoun, adjective, object);
